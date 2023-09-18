5eb9d8791e51812da07708097d5321e47837fa23
Venkata Lakshman Nimmagadda <lakshman@Venkatas-Air.lan>
2023-09-17 20:14:38 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http'); // Import the http module if you plan to create an HTTP server.
 const app = express();
+const budget = require("./data.json");
 const port = 3000;
 
 app.use ('/', express.static('public'));
-
-const budget =  {
-    myBudget: [
-{
-    title: 'Eat out',
-    budget: 25
-},
-{
-    title: 'Rent',
-    budget: 375
-},
-{
-    title: 'Grocery',
-    budget: 110
-},
-]
-};
 
 app.get('/hello', (req, res) => {
 res.send('Hello World!');
